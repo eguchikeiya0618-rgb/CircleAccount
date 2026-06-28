f0acaf44dd3ec30ff7e37ad1aad6ef7e22bca563
江口　恵哉 <k_gbad_keiya@yahoo.co.jp>
2026-06-28 12:11:00 +0900

    Initial Commit

---
+++ CircleAccount/CircleAccountApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleAccountApp.swift
+//  CircleAccount
+//
+//  Created by 江口恵哉 on 2026/06/27.
+//
+
+import SwiftUI
+
+@main
+struct CircleAccountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircleAccount/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CircleAccount
+//
+//  Created by 江口恵哉 on 2026/06/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
